ad446a620758d3ad342a08df559c6bb7f068fe04
allthelucky <winfirm@163.com>
2014-06-24 16:23:20 +0800

    remove

--- AndroidVolley/AndroidVolley/src/com/android/http/LoadListener.java
+++
@@ -1,7 +0,0 @@
-package com.android.http;
-
-public interface LoadListener {
-	void onStart();
-	void onSuccess(byte[] data, String url, int actionId);
-	void onError(String errorMsg, String url, int actionId);
-}
