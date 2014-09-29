9b614fe6bfc55b548821df0a66202c90e4da8bf6
panxw <panxw@fun.tv>
2014-09-29 17:45:01 +0800

    update BitmapHelper

--- CodeSnippetProject/src/com/allthelucky/examples/common/BitmapHelper.java
+++ CodeSnippetProject/src/com/allthelucky/examples/common/BitmapHelper.java
@@ -2,14 +2,21 @@ package com.allthelucky.examples.common;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.PixelFormat;
 import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 
 public final class BitmapHelper {
@@ -119,4 +126,59 @@ public final class BitmapHelper {
         Bitmap bitmap = view.getDrawingCache(true);
         return bitmap;
 	}
+	
+	/**
+	 * save drawable to file
+	 * 
+	 * @param context
+	 * @param drawable
+	 * @param name
+	 */
+	public static void savePic(Context context, Drawable drawable, String filePath) {
+		Bitmap b = drawableToBitamp(drawable);
+
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(new File(filePath));
+			if (null != fos) {
+				b.compress(Bitmap.CompressFormat.PNG, 100, fos);
+				fos.flush();
+				fos.close();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * getBitmap from Drawable
+	 * 
+	 * @param drawable
+	 */
+	public static Bitmap drawableToBitamp(Drawable drawable) {
+		BitmapDrawable bd = (BitmapDrawable) drawable;
+		return bd.getBitmap();
+	}
+
+	/**
+	 * create new Bitmap from Drawable
+	 * 
+	 * @param drawable
+	 */
+	public static Bitmap drawableToBitampb(Drawable drawable) {
+		int w = drawable.getIntrinsicWidth();
+		int h = drawable.getIntrinsicHeight();
+		System.out.println("DrawableתBitmap");
+		Bitmap.Config config = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+				: Bitmap.Config.RGB_565;
+		Bitmap bitmap = Bitmap.createBitmap(w, h, config);
+		Canvas canvas = new Canvas(bitmap);
+		drawable.setBounds(0, 0, w, h);
+		drawable.draw(canvas);
+
+		return bitmap;
+	}
+
 }
